8faab835d1d2f6168e2ce2143e3bef010ee8c5a0
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2025-08-12 20:46:53 +0545

    Remove environment variable files and add initial migrations for Client and Product models

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django_tenants.models import TenantMixin, DomainMixin
+
+
+class Client(TenantMixin):
+    name = models.CharField(max_length=100)
+    paid_until = models.DateField(null=True, blank=True)
+    on_trial = models.BooleanField(default=True)
+    # schema_name is required by TenantMixin
+    # auto-create schema on save()
+
+
+class Domain(DomainMixin):
+    pass
